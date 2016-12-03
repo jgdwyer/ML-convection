d297dcf0e0048847ef4cdc56aba2f392bf3ed222
John Dwyer <jgdwyer@gmail.com>
2016-12-02 16:46:51 -0800

    Updated workbook and run.py scripts

--- run.py
+++ run.py
@@ -7,36 +7,34 @@ x_ppi={'name':'StandardScaler','method':'qTindividually'}
 y_ppi={'name':'SimpleY','method':'qTindividually'}
 #y_ppi={'name':'MaxAbs','method':'qTindividually'}
 
-n_samps = [100,200,500,1000,2000,3500,5000,7500,10000,12500,15000]
 
-#hid_neur = [10,20,40,60,100,150,225,300,450,600,800,1000]
-# hid_neur = [5,15,30,50,80]
-# nntrain.train_nn_wrapper(2, 61, x_ppi, y_ppi, n_iter=10000, minlev=0.25, noshallow=True)
-for n_samp in n_samps:
-    nntrain.train_nn_wrapper(2, 60, x_ppi, y_ppi, minlev=0.25, n_iter=10000,
-                            rainonly=False,  use_weights=False, weight_decay=0.0,
-                            N_trn_exs=n_samp)
-# for h in hid_neur:
-#     r_str = nntrain.train_nn_wrapper(1, h, x_ppi, y_ppi, n_iter=10000, minlev=0.25,
-#      use_weights=False, weight_decay=0.0)
-#     r_str = nntrain.train_nn_wrapper(1, h, x_ppi, y_ppi, n_iter=10000, minlev=0.25,
-#      use_weights=False, weight_decay=0.00001)
-#     r_str = nntrain.train_nn_wrapper(2, h, x_ppi, y_ppi, n_iter=10000, minlev=0.25,
-#      use_weights=False, weight_decay=0.0)
-#     r_str = nntrain.train_nn_wrapper(2, h, x_ppi, y_ppi, n_iter=10000, minlev=0.25,
-#      use_weights=False, weight_decay=0.00001)
-#r_str[0] = nntrain.train_nn_wrapper(4, 200, x_ppi, y_ppi, n_iter=5000, minlev=0.25, use_weights=True, weight_decay=0.00001)
-#r_str[0] = nntrain.train_nn_wrapper(4, 1000, x_ppi, y_ppi, n_iter=4000, minlev=0.25, use_weights=True, weight_decay=0.0)
-#r_str[1] = nntrain.train_nn_wrapper(50, x_ppi, y_ppi, n_iter=1000, minlev=0.25, use_weights=True, weight_decay=0.00001)
-#r_str[2] = nntrain.train_nn_wrapper(50, x_ppi, y_ppi, n_iter=1000, minlev=0.25, use_weights=True, weight_decay=0.0001)
-#r_str[3] = nntrain.train_nn_wrapper(50, x_ppi, y_ppi, n_iter=1000, minlev=0.25, use_weights=True, weight_decay=0.001)
-#r_str[4] = nntrain.train_nn_wrapper(50, x_ppi, y_ppi, n_iter=1000, minlev=0.25, use_weights=True, weight_decay=0.01)
-#r_str[5] = nntrain.train_nn_wrapper(50, x_ppi, y_ppi, n_iter=1000, minlev=0.25, use_weights=True, weight_decay=0.1)
-#r_str[6] = nntrain.train_nn_wrapper(50, x_ppi, y_ppi, n_iter=1000, minlev=0.25, use_weights=True, weight_decay=1.0)
-#r_str[7] = nntrain.train_nn_wrapper(50, x_ppi, y_ppi, n_iter=1000, minlev=0.25, use_weights=True, weight_decay=10.0)
-#r_str[8] = nntrain.train_nn_wrapper(50, x_ppi, y_ppi, n_iter=1000, minlev=0.25, use_weights=True, weight_decay=100.0)
-#r_str[1], x, y = nntrain.train_nn_wrapper(800, x_ppi, y_ppi, n_stable=100, minlev=0.25)
-#r_str[2], x, y = nntrain.train_nn_wrapper(1200, x_ppi, y_ppi, n_stable=100)
-#r_str[3], x, y = nntrain.train_nn_wrapper(1600, x_ppi, y_ppi, n_stable=100)
+num_layers=[1, 2]
+num_neurons = [10, 50, 100]
+num_trains = [1000, 5000, 10000, 100000]
+cvcdbool = [False, True]
+regs = [1e-5, 1e-6]
 
-#nnplot.plot_all_figs('X-StandardScaler-indivi_Y-MaxAbs-qTindi_r_50R_mom0.5reg0.001_w2')
+for cvcd in cvcdbool:
+    for reg in regs:
+        for num_layer in num_layers:
+            for num_train in num_trains:
+                for num_neuron in num_neurons:
+                    nntrain.train_nn_wrapper(num_layer, num_neuron, x_ppi, y_ppi, minlev=0.2,
+                                         n_iter=10000, rainonly=False, weight_decay=reg,
+                    N_trn_exs=num_train, weight_precip=False, weight_shallow=False, convcond=cvcd)
+
+
+# n_samps = [1000,5000,10000,50000]
+# # n_samps=[1001]
+# #hid_neur = [10,20,40,60,100,150,225,300,450,600,800,1000]
+# # hid_neur = [5,15,30,50,80]
+# # nntrain.train_nn_wrapper(2, 61, x_ppi, y_ppi, n_iter=10000, minlev=0.25, noshallow=True)
+# for n_samp in n_samps:
+#     nntrain.train_nn_wrapper(2, 60, x_ppi, y_ppi, minlev=0.1, n_iter=10000,
+#                         rainonly=False, weight_decay=1e-4,
+#                         N_trn_exs=n_samp, weight_precip=False, weight_shallow=False)
+# for n_samp in n_samps:
+#     nntrain.train_nn_wrapper(2, 60, x_ppi, y_ppi, minlev=0.1, n_iter=10000,
+#                         rainonly=False, weight_decay=1e-4,
+#                         N_trn_exs=n_samp, weight_precip=False, weight_shallow=False,
+#                         convcond=True)
